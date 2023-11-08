6b782a54107266fd3b13beded4f2aeb853b27168
Ashok_dobariya <ashokpdobariya2081978@gmail.com>
2023-11-08 11:32:02 +0530

    upadatea

--- src/registration.php
+++ src/registration.php
@@ -8,4 +8,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Hyva_LumaCheckout', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Hyva_QtyManager', __DIR__);
